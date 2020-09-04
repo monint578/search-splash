7c19f0b4aec1a3196955b3546610555e996bbe39
Monika <monint96@gmail.com>
2020-09-04 10:03:18 +0300

    Updates

---
+++ precache-manifest.00323f500ba98dc5c8a03bf91ff29f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f49b192b50000513ee929fdb1883da",
+    "url": "/search-splash/index.html"
+  },
+  {
+    "revision": "8b08d54759f5843600d5",
+    "url": "/search-splash/static/css/main.2b52a2f6.chunk.css"
+  },
+  {
+    "revision": "dd0bd22891122a09b5ef",
+    "url": "/search-splash/static/js/2.0a1f80b5.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/search-splash/static/js/2.0a1f80b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b08d54759f5843600d5",
+    "url": "/search-splash/static/js/main.f499ff61.chunk.js"
+  },
+  {
+    "revision": "58e7469e7ea9d9ce01cc",
+    "url": "/search-splash/static/js/runtime-main.bdc3d06e.js"
+  }
+]);
